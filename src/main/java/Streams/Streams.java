c2ca8e8d5b698ea6f220427a6537619014dd015b
ViktoriaAve <73614080+ViktoriaAve@users.noreply.github.com>
2023-02-18 11:17:34 +0200

    Added "Streams" package

---
+++ src/main/java/Streams/Streams.java
@@ -0,0 +1,10 @@
+package Streams;
+
+public class Streams {
+    public static void main(String[] args) {
+        //    Streams - Exercise 3:
+        //    Write a method that returns the average of a list of integers using streams
+
+
+    }
+}
